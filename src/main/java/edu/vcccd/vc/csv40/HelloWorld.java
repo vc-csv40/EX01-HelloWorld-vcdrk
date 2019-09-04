0ccd6924e6acfea6cc8ec1d183f66215b1e6c7e7
aknight <aknight@vcccd.edu>
2019-09-04 16:37:07 -0700

    Turning it in

---
+++ src/main/java/edu/vcccd/vc/csv40/HelloWorld.java
@@ -0,0 +1,16 @@
+package edu.vcccd.vc.csv40;
+/**
+ * CS V40 Beginning Java
+ * Assignment: edu.vcccd.vc.csv40.HelloWorld
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @author [CHANGE THIS TO YOUR INFORMATION]
+ */
+public class HelloWorld {
+
+public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        }
+}
